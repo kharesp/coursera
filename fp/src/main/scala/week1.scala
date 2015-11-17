be90a1417dd77d6f57e440f9775473deaebdcfae
kharesp <kharesp28@gmail.com>
2015-11-16 20:26:16 -0600

    second draft

--- fp/src/main/scala/week1.scala
+++ fp/src/main/scala/week1.scala
@@ -1,47 +1,66 @@
-/** Non-terminating expression */
-def loop:Int = loop 
+object week1 extends App{
+	/** Non-terminating expression */
+	def loop:Boolean= loop 
+	
+	/** Value definition by name
+	  * RHS is evaluated every time the variable is used 
+	  */
+	def y:Boolean= loop
+	
+	/** Value definition by value. 
+	  * Value is only evaluated once at the time of definition
+	  * Following will not terminate:
+	  * val x:Boolean=loop 
+	  */
+	
+	/** Function arguments can either be passed by value(default)
+	  * or by name. Arguments passed by value are evaluted first 
+	  * before the function is evaluated. If arguments are passed 
+	  * by name, the function is applied to un-reduced arguments. 
+	  * => is used to denote that an argument is passed by name
+	  */
+	
+	/** Passing second function argument by name */
+	def and(x:Boolean,y: => Boolean):Boolean = if (x) y else false 
+	def or(x:Boolean,y: => Boolean):Boolean = if(x) true else y
+	
+	/** As second function argument is passed by name, the following 
+	  * expression gets evaluated else it would not terminate.
+	  */
+	val result = and(false,loop) 
+	
+	def abs(x:Double):Double = if (x>=0) x else -x 
+	
+	def sqrt(x:Double):Double = {
+		def isGoodEnough(guess:Double):Boolean = 
+			abs(guess*guess - x)/x < 0.001
+	
+		def improve(guess:Double):Double = 
+			(guess + x/guess)/2 
+	
+		def sqrtIter(guess:Double):Double = 
+			if (isGoodEnough(guess)) guess 
+			else sqrtIter(improve(guess))
+	
+		sqrtIter(1.0)
+	}
+	println("Square root of 2:"+sqrt(2))
+	
+	/** Tail Recursive gcd (greatest common divisor) function */
+	def gcd(a:Int,b:Int):Int= if(b==0) a else gcd(b,a%b)
+	
+	println("GCD of 21,14 is:"+gcd(21,14))
+	
+	/** Non Tail recursive factorial function */
+	def factorial(n:Int):Int= if(n==0) 1 else n*factorial(n-1)
+	
+	/** Tail recursive factorial function */
+	def tailRecursiveFactorial(n:Int):Int={
+		def loop(acc:Int,n:Int):Int =
+			if(n==0) acc 
+			else loop(acc*n,n-1)
+		loop(1,n)
+	}
+	println("factorial of 5:"+tailRecursiveFactorial(5))
 
-/** Value definition by name
-  * RHS is evaluated every time the variable is used 
-  */
-def y:Int = loop
-
-/** Value definition by value. 
-  * Value is only evaluated once at the time of definition
-  * Following will not terminate:
-  * val x:Int =loop 
-  */
-
-/** Function arguments can either be passed by value(default)
-  * or by name. Arguments passed by value are evaluted first 
-  * before the function is evaluated. If arguments are passed 
-  * by name, the function is applied to un-reduced arguments. 
-  * => is used to denote that an argument is passed by name
-  */
-
-/** Passing second function argument by name */
-def and(x:Boolean,y: => Boolean):Boolean = if (x) y else false 
-def or(x:Boolean,y: => Boolean):Boolean = if(x) true else y
-
-/** As second function argument is passed by name, the following 
-  * expression gets evaluated else it would not terminate.
-  */
-val result = and(false,loop) 
-
-def abs(x:Double):Double = if (x>=0) x else -x 
-
-def sqrt(x:Double):Double = {
-	def isGoodEnough(guess:Double):Boolean = 
-		abs(guess*guess - x)/x < 0.001
-
-	def improve(guess:Double):Double = 
-		(guess + x/guess)/2 
-
-	def sqrtIter(guess:Double):Double = 
-		if (isGoodEnough(guess)) guess 
-		else sqrtIter(improve(guess),x)
-
-	sqrtIter(1.0,x)
 }
-
-
--- fp/src/main/scala/week2.scala
+++ fp/src/main/scala/week2.scala
@@ -1,30 +1,49 @@
-/** Higher-Order Function accepting another function */
-def sum(f:Int=>Int,a:Int,b:Int):Int={
-	def loop(acc:Int,n:Int):Int=
-		if(n>b) acc else loop(f(n)+acc,n+1)
-	loop(0,a)
+object week2 extends App{
+	
+	/** Higher-Order Function accepting another function */
+	def sum(f:Int=>Int,a:Int,b:Int):Int={
+		def loop(acc:Int,n:Int):Int=
+			if(n>b) acc else loop(f(n)+acc,n+1)
+		loop(0,a)
+	}
+	println("Sum of integers from 1 to 5: "+sum(x=>x,1,5))
+	
+	/** Curried notation of function returning another function */
+	def sumCurried(f:Int=>Int)(a:Int,b:Int):Int=
+		if(a>b) 0 else f(a)+sumCurried(f)(a+1,b)
+	sumCurried(x=>x)(1,5)
+	println("Sum of integers from 1 to 5: "+sumCurried(x=>x)(1,5))
+	
+	/** Curried mapreduce function */
+	def mapreduce(map:Int=>Int,reduce:(Int,Int)=>Int,zero:Int)(a:Int,b:Int):Int=
+		if(a>b) zero 
+		else reduce(map(a),mapreduce(map,reduce,zero)(a+1,b))
+	
+	/** Define product in terms of mapreduce */
+	def product(f:Int=>Int)(a:Int,b:Int):Int=
+		mapreduce(f,(x,y)=>x*y,1)(a,b)
+	println("Product of integers from 1 to 5: "+product(x=>x)(1,5))
+
+	def abs(x:Double):Double=
+		if (x<0) -x else x
+
+	/** Fixed point of a function */
+	def fixedPoint(f:Double=>Double)(init:Double)={
+		val tolerance:Double=0.0001
+		def closeEnough(x:Double,y:Double):Boolean=
+			abs(x-y)/x<tolerance 
+		def iterate(guess:Double):Double={
+			val next=f(guess)
+			if (closeEnough(guess,next)) next 
+			else iterate(next)
+		}
+		iterate(init)
+	}
+	println("Fixed point of f(x)=1+x/2: "+fixedPoint(x=>1+x/2)(1))
+
+	def averageDamp(f:Double=>Double)(x:Double)=(x+f(x))/2	
+
+	/** sqrt function in terms of fixed point */
+	def sqrt(x:Double):Double=fixedPoint(averageDamp(y=>x/y))(1)
+	println("Sqrt of 2: "+sqrt(2))
 }
-sum(x=>x,1,5)
-
-/** function returning another function */
-def sumReturningFunction(f:Int=>Int):(a:Int,b:Int)=>Int={
-	def sumF(a:Int,b:Int):Int=
-		if(a>b) 0 else f(a)+sumF(a+1,b)
-	sumF
-}
-sumReturningFunction(x=>x)(1,5)
-
-/** Curried notation of function returning another function */
-def sumCurried(f:Int=>Int)(a:Int,b:Int):Int=
-	if(a>b) 0 else f(a)+sum(f)(a+1,b)
-sumCurried(x=>x)(1,5)
-
-
-/** Curried mapreduce function */
-def mapreduce(map:Int=>Int,reduce:(Int,Int)=>Int,zero:Int)(a:Int,b:Int):Int=
-	if(a>b) zero 
-	else reduce(map(a),mapreduce(map,reduce,zero)(a+1,b))
-
-/** Define product in terms of mapreduce */
-def product(f:Int=>Int)(a:Int,b:Int):Int=
-	mapreduce(f,(x:Int,y:Int):Int=>x*y,1)(a,b)
